fd6536482f6e46417532a6c95d95ba5ccb8c00a4
Santiago Joaquin Diaz <santiagodiaz.arg@gmail.com>
2025-03-27 04:33:55 -0300

    Update MenuButtonToggle to use MenuIcon for closed state; improve icon handling

--- src/components/ui/atoms/MenuButtonToggle.tsx
+++ src/components/ui/atoms/MenuButtonToggle.tsx
@@ -1,5 +1,6 @@
-import { IconButton as MUIIconButton } from "@mui/material";
-import MenuOpenIcon from "@mui/icons-material/MenuOpen";
+import { IconButton as MUIIconButton } from '@mui/material';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
+import MenuIcon from '@mui/icons-material/Menu';
 
 interface IconButtonProps {
   isOpen: boolean;
@@ -8,7 +9,7 @@ interface IconButtonProps {
 
 const MenuButtonToggle: React.FC<IconButtonProps> = ({ isOpen, onClick }) => (
   <MUIIconButton color="inherit" aria-label="toggle menu" onClick={onClick} edge="start">
-    {isOpen ? <MenuOpenIcon /> : <MenuOpenIcon />}
+    {isOpen ? <MenuOpenIcon /> : <MenuIcon />}
   </MUIIconButton>
 );
 
