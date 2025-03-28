56fe792ad37b0e27a7c55f00778ff4c4c05d7d0b
JackmoonGit <santiagodiaz.arg@gmail.com>
2025-03-28 12:24:24 -0300

    Refactor LoginPage for improved readability; enhance error handling and loading states in RedirectToBackoffice

--- src/modules/portal/RedirectToBackoffice.tsx
+++ src/modules/portal/RedirectToBackoffice.tsx
@@ -1,5 +1,9 @@
-import { Navigate } from "react-router";
+import FullScreenLoader from '@/components/ui/molecules/FullScreenLoader';
+import SkeletonLoader from '@/components/ui/molecules/SkeletonLoader';
+import { Navigate, useNavigation } from 'react-router';
 
 export default function RedirectToBackoffice() {
-  return <Navigate to="/backoffice" replace />;
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
+  return isNavigating ? <FullScreenLoader open /> : <Navigate to="/backoffice" replace />;
 }
