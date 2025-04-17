4ef6c22df142148c10f46f89e7300d2a07cf8a73
JackmoonGit <santiagodiaz.arg@gmail.com>
2025-04-17 01:51:05 -0300

    Enhance ResetPasswordPage: improve password validation with criteria checks, add visibility toggle for password fields, implement error handling with ErrorModal2, and display error messages for better user feedback.

--- src/modules/auth/pages/ResetPasswordPage.tsx
+++ src/modules/auth/pages/ResetPasswordPage.tsx
@@ -3,19 +3,39 @@ import useAuthQueries from '../hooks/useAuthQueries';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { FormProvider, useForm } from 'react-hook-form';
-import { Box, Button } from '@mui/material';
+import { Box, Button, List, ListItem, ListItemText, InputAdornment, IconButton } from '@mui/material';
 import TextFieldForm from '@/components/ui/atoms/TextFieldForm';
-import { Check, Close } from '@mui/icons-material';
+import { Check, Close, Visibility, VisibilityOff } from '@mui/icons-material';
+import FailedToLoadData from '@/components/ui/molecules/FailedToLoadData';
+import ErrorModal2 from '@/components/ui/molecules/ErrorModal2';
+import { useState } from 'react';
+import { useErrorStore } from '@/stores';
 
 interface IResetPasswordForm {
   newPassword: string;
   confirmPassword: string;
 }
 
-const resetPasswordValidationSchema = z.object({
-  newPassword: z.string().min(8, 'Password must be at least 8 characters long'),
-  confirmPassword: z.string().min(8, 'Confirm Password must be at least 8 characters long'),
-});
+const resetPasswordValidationSchema = z
+  .object({
+    newPassword: z
+      .string()
+      .min(8, 'At least 8 characters')
+      .refine((val) => /[A-Z]/.test(val), {
+        message: 'At least one uppercase letter',
+      })
+      .refine((val) => /[a-z]/.test(val), {
+        message: 'At least one lowercase letter',
+      })
+      .refine((val) => /\d/.test(val), {
+        message: 'At least one number',
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.newPassword === data.confirmPassword, {
+    message: 'Passwords do not match',
+    path: ['confirmPassword'],
+  });
 
 export default function ResetPasswordPage() {
   const location = useLocation();
@@ -30,33 +50,62 @@ export default function ResetPasswordPage() {
 
   const {
     handleSubmit,
+    watch,
     formState: { errors },
   } = methods;
 
-  const onSubmitForm = async (data: IResetPasswordForm) => {
-    if (data.newPassword !== data.confirmPassword) {
-      methods.setError('confirmPassword', { type: 'manual', message: 'Passwords do not match' });
-      return;
-    }
+  const password = watch('newPassword', '');
+  // const [isValidationErrorModalOpen, setIsValidationErrorModalOpen] = useState(false);
+  // Estados para mostrar/ocultar contraseña
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const { setOpenModal, openModal, clearError } = useErrorStore();
 
-    if (!resetToken) {
-      // Handle the case where the token is missing (e.g., show an error message)
-      console.error('Reset token is missing');
-      return;
-    }
-
-    resetPasswordMutation.mutateAsync(
-      { token: resetToken, newPassword: data.newPassword },
-      {
-        onSuccess: () => {
-          // Handle success (e.g., redirect to login page)
-        },
-        onError: () => {
-          // Handle error (e.g., show error message)
-        },
-      },
-    );
+  const getErrorMessages = (errors: any): string[] => {
+    const messages: string[] = [];
+    const iterate = (errObj: any) => {
+      if (errObj?.message) messages.push(errObj.message);
+      if (errObj && typeof errObj === 'object') {
+        for (const key in errObj) {
+          if (typeof errObj[key] === 'object') iterate(errObj[key]);
+        }
+      }
+    };
+    iterate(errors);
+    return messages;
   };
+
+  if (!resetToken) {
+    console.error('Reset token is missing');
+    return (
+      <Box sx={{ width: 600, mt: 2 }}>
+        <FailedToLoadData secondaryText="Reset token is missing" />
+      </Box>
+    );
+  }
+
+  const onSubmitForm = handleSubmit(
+    async (data: IResetPasswordForm) => {
+      if (data.newPassword !== data.confirmPassword) {
+        methods.setError('confirmPassword', { type: 'manual', message: 'Passwords do not match' });
+      }
+
+      resetPasswordMutation.mutateAsync(
+        { token: resetToken, newPassword: data.newPassword },
+        {
+          onSuccess: () => {
+            // Handle success (e.g., redirect to login page)
+          },
+          onError: () => {
+            // Handle error (e.g., show error message)
+          },
+        },
+      );
+    },
+    (errors) => {
+      if (Object.keys(errors).length > 0) setOpenModal(true);
+    },
+  );
 
   const passwordCriteria = {
     isLengthValid: {
@@ -80,9 +129,41 @@ export default function ResetPasswordPage() {
   return (
     <>
       <FormProvider {...methods}>
-        <Box component="form" onSubmit={handleSubmit(onSubmitForm)} mt={3} sx={{ width: '100%', maxWidth: 400, mx: 'auto' }}>
-          <TextFieldForm name="newPassowrd" label="Password" />
-          <TextFieldForm name="confirmPassword" label="Confirm Password" />
+        <Box component="form" onSubmit={onSubmitForm} mt={3} sx={{ width: '100%', maxWidth: 400, mx: 'auto' }}>
+          <TextFieldForm
+            name="newPassword"
+            label="Password"
+            type={showPassword ? 'text' : 'password'}
+            variant="outlined"
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton aria-label={showPassword ? 'Hide password' : 'Show password'} onClick={() => setShowPassword((show) => !show)} edge="end">
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
+          <TextFieldForm
+            name="confirmPassword"
+            label="Confirm Password"
+            type={showConfirmPassword ? 'text' : 'password'}
+            variant="outlined"
+            slotProps={{
+              input: {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton aria-label={showConfirmPassword ? 'Hide password' : 'Show password'} onClick={() => setShowConfirmPassword((show) => !show)} edge="end">
+                      {showConfirmPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              },
+            }}
+          />
           <Box mt={2}>
             {Object.entries(passwordCriteria).map(([key, criteria]) => (
               <Box display="flex" alignItems="center" key={key}>
@@ -97,6 +178,21 @@ export default function ResetPasswordPage() {
             </Button>
           </Box>
         </Box>
+        <ErrorModal2
+          open={openModal}
+          onClose={() => {
+            setOpenModal(false);
+            clearError();
+          }}
+        >
+          <List sx={{ padding: 0, margin: 0 }}>
+            {getErrorMessages(methods.formState.errors).map((msg, idx) => (
+              <ListItem key={idx} disableGutters sx={{ padding: '1px 0' }}>
+                <ListItemText primary={`• ${msg}`} sx={{ margin: 0, padding: 0 }} />
+              </ListItem>
+            ))}
+          </List>
+        </ErrorModal2>
       </FormProvider>
     </>
   );
