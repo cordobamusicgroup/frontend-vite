ef8afa7e6e8370176e0c695538259d0d64c71424
Jackmoon <santiagodiaz.arg@gmail.com>
2025-06-18 17:28:19 -0300

    Add countdown title update in session dialog

--- src/components/ui/molecules/SessionTimeoutDialog.tsx
+++ src/components/ui/molecules/SessionTimeoutDialog.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from '@mui/material';
 
 interface SessionTimeoutDialogProps {
@@ -9,8 +9,34 @@ interface SessionTimeoutDialogProps {
 }
 
 const SessionTimeoutDialog: React.FC<SessionTimeoutDialogProps> = ({ open, countdown, onStayLoggedIn, onLogout }) => {
-  return (
-    <Dialog open={open} onClose={onLogout} maxWidth="xs" fullWidth>
+  const originalTitleRef = useRef<string>('');
+
+  useEffect(() => {
+    if (open) {
+      originalTitleRef.current = document.title;
+    }
+  }, [open]);
+
+  useEffect(() => {
+    if (open) {
+      document.title = `Session expiring in ${countdown}s`;
+    } else if (originalTitleRef.current) {
+      document.title = originalTitleRef.current;
+    }
+  }, [open, countdown]);
+
+  useEffect(() => {
+    return () => {
+      if (originalTitleRef.current) {
+        document.title = originalTitleRef.current;
+      }
+    };
+  }, []);
+
+    return (
+      <>
+        {open && <title>Session expiring in {countdown}s</title>}
+        <Dialog open={open} onClose={onLogout} maxWidth="xs" fullWidth>
       <DialogTitle>Session Expiring</DialogTitle>
       <DialogContent>
         <Typography variant="body2" mb={2}>
@@ -26,8 +52,9 @@ const SessionTimeoutDialog: React.FC<SessionTimeoutDialogProps> = ({ open, count
           Stay Logged In
         </Button>
       </DialogActions>
-    </Dialog>
-  );
+        </Dialog>
+      </>
+    );
 };
 
 export default SessionTimeoutDialog;
