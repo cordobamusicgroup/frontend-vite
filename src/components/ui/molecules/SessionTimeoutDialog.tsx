a2df0852575097cf152d60fe5628bba83360c5b9
JackmoonGit <santiagodiaz.arg@gmail.com>
2025-06-18 17:32:45 -0300

    refactor(SessionTimeoutDialog): remove unused title management logic

--- src/components/ui/molecules/SessionTimeoutDialog.tsx
+++ src/components/ui/molecules/SessionTimeoutDialog.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, Button, Typography } from '@mui/material';
 
 interface SessionTimeoutDialogProps {
@@ -9,52 +9,28 @@ interface SessionTimeoutDialogProps {
 }
 
 const SessionTimeoutDialog: React.FC<SessionTimeoutDialogProps> = ({ open, countdown, onStayLoggedIn, onLogout }) => {
-  const originalTitleRef = useRef<string>('');
-
-  useEffect(() => {
-    if (open) {
-      originalTitleRef.current = document.title;
-    }
-  }, [open]);
-
-  useEffect(() => {
-    if (open) {
-      document.title = `Session expiring in ${countdown}s`;
-    } else if (originalTitleRef.current) {
-      document.title = originalTitleRef.current;
-    }
-  }, [open, countdown]);
-
-  useEffect(() => {
-    return () => {
-      if (originalTitleRef.current) {
-        document.title = originalTitleRef.current;
-      }
-    };
-  }, []);
-
-    return (
-      <>
-        {open && <title>Session expiring in {countdown}s</title>}
-        <Dialog open={open} maxWidth="xs" fullWidth disableEscapeKeyDown>
-      <DialogTitle>Session Expiring</DialogTitle>
-      <DialogContent>
-        <Typography variant="body2" mb={2}>
-          Your session will expire soon. Do you want to stay logged in?
-        </Typography>
-        <Typography variant="body2">This window will close in {countdown} seconds.</Typography>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onLogout} color="secondary">
-          Logout
-        </Button>
-        <Button onClick={onStayLoggedIn} color="primary" variant="contained">
-          Stay Logged In
-        </Button>
-      </DialogActions>
-        </Dialog>
-      </>
-    );
+  return (
+    <>
+      {open && <title>Session expiring in {countdown}s</title>}
+      <Dialog open={open} maxWidth="xs" fullWidth disableEscapeKeyDown>
+        <DialogTitle>Session Expiring</DialogTitle>
+        <DialogContent>
+          <Typography variant="body2" mb={2}>
+            Your session will expire soon. Do you want to stay logged in?
+          </Typography>
+          <Typography variant="body2">This window will close in {countdown} seconds.</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onLogout} color="secondary">
+            Logout
+          </Button>
+          <Button onClick={onStayLoggedIn} color="primary" variant="contained">
+            Stay Logged In
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
 };
 
 export default SessionTimeoutDialog;
